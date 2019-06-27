d1cdb01ea7e979fcf0105135a38e0b97ec751c2b
eeieeipoo <jakebonk@gmail.com>
2019-06-27 11:03:47 -0500

    kjhkjhk

--- webpmux.js
+++ webpmux.js
@@ -4,15 +4,15 @@ var knowos=function()
 {
 if (process.platform === 'darwin') {
 
-    return  __dirname.replace(/\\/g,"/") + '/lib/osx/bin/webpmux';
+    return  path.join(__dirname, 'lib/osx/bin/webpmux');
 
 }else if (process.platform === 'linux') {
 
-   return  __dirname.replace(/\\/g,"/") + '/lib/linux/bin/webpmux';
+   return  path.join(__dirname, 'lib/linux/bin/webpmux');
 
 }else if (process.platform === 'win32') {
     
-   return __dirname.replace(/\\/g,"/") + '/lib/win32/bin/webpmux.exe';
+   return path.join(__dirname, 'lib/win32/bin/webpmux.exe');
 
 } else {
     console.log('Unsupported platform:', process.platform, process.arch);
