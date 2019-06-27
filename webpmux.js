65ade265538215dc2d2964b588529f954d0a04ae
eeieeipoo <jakebonk@gmail.com>
2019-06-27 00:28:00 -0500

    commit 17

--- webpmux.js
+++ webpmux.js
@@ -4,15 +4,15 @@ var knowos=function()
 {
 if (process.platform === 'darwin') {
 
-    return  path.join(__dirname, '/lib/osx/bin/webpmux');
+    return  './lib/osx/bin/webpmux';
 
 }else if (process.platform === 'linux') {
 
-   return  path.join(__dirname, '/lib/linux/bin/webpmux');
+   return  './lib/linux/bin/webpmux';
 
 }else if (process.platform === 'win32') {
     
-   return  path.join(__dirname, '/lib/win32/bin/webpmux.exe');
+   return './lib/win32/bin/webpmux.exe';
 
 } else {
     console.log('Unsupported platform:', process.platform, process.arch);
